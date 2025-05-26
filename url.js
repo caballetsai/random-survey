c9c2ad6268cbb2406677c4739c94443c016ab90d
caballetsai <156987059+caballetsai@users.noreply.github.com>
2025-05-26 20:29:53 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/rGbn0',
+	'https://www.surveycake.com/s/4eaB6,
 ];
